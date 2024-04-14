191343ca4cfc66ccd695a27103178c16e8e35b48
Hoang Son <son.lyhoang2014@gmail.com>
2024-04-14 10:44:26 +0700

    Install Morgan

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express') // Thư viện chúng ta vừa cài
+const morgan = require('morgan') // Thư viện morgan chúng ta vừa cài
+
 const app = express() // Trả về 1 Instance của express()
-// chúng ta sẽ sử dụng nó xuyên suốt trong quá trình
+// Chúng ta sẽ sử dụng nó xuyên suốt trong quá trình
 const PORT = 3000 // Khai báo biến PORT có giá trị 3000
+
+//Sử dụng morgan
+app.use(morgan(''))
 
 //Định nghĩa các route
 app.get('/',function(req,res){
-    res.send('Hello world')
+    res.send('<h1>Hello world</h1>')
 })
 
 app.listen(PORT,()=>{
